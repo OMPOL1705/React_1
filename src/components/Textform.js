43e44a1c0cc3ac52b2c727371d117453d85c9a31
OM1705 <ompol1705@gmail.com>
2023-06-17 18:36:57 +0530

    Initial commit

---
+++ src/components/Textform.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+export default function Textform() {
+  const change = (event) => {
+    setText(event.target.value);
+  };
+
+  const clear = () => {
+    let newtext = "";
+    setText(newtext);
+  };
+
+  const toupper = () => {
+    let newtext = text.toUpperCase();
+    setText(newtext);
+  };
+
+  const tolower = () => {
+    let newtext = text.toLowerCase();
+    setText(newtext);
+  };
+
+  const space = () => {
+    let newtext = text.replaceAll(/ /g, "");
+    setText(newtext);
+  };
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <form>
+        <h1>Enter the text</h1>
+        <div className="form-group">
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="7"
+            value={text}
+            onChange={change}
+          ></textarea>
+        </div>
+      </form>
+      <button type="button" onClick={clear} className="btn btn-primary mx-3">
+        Clear
+      </button>
+      <button type="button" onClick={toupper} className="btn btn-primary mx-3">
+        Upper Case
+      </button>
+      <button type="button" onClick={tolower} className="btn btn-primary mx-3">
+        Lower Case
+      </button>
+      <button type="button" onClick={space} className="btn btn-primary mx-3">
+        Space Remover
+      </button>
+      <div className="container my-3">
+        <h2>Summary of data</h2>
+        <p>{text.split(" ").length} words</p>
+        <p>{text.length + 1 - text.split(" ").length} charcters</p>
+      </div>
+    </>
+  );
+}
