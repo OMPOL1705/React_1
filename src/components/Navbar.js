131e184a0c57bf89e825adaad7d1f6471c80e7dc
OM1705 <ompol1705@gmail.com>
2023-06-21 14:22:18 +0530

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ export default function Navbar(props) {
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link className="nav-link" to="/">
                   {props.new}
                   <span className="sr-only">(current)</span>
